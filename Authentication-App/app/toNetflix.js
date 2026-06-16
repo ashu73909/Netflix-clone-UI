f6cec8ec2e5c9128d345e1c685f9ffa04f2fbf5c
ashu73909 <156508561+ashu73909@users.noreply.github.com>
2026-06-16 16:05:11 +0530

    comment

--- Authentication-App/app/toNetflix.js
+++ Authentication-App/app/toNetflix.js
@@ -3,6 +3,7 @@ document.addEventListener('click',(e)=>{
     const button=e.target.classList.contains("movie-site");
     if(!button) return;
     loader.classList.remove("hidden");
+    //refers back to the index page 
     window.location.href="../../index.html";
     loader.classList.add("hidden");
 });
